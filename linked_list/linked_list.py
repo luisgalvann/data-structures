08f0d22a9ca33f5f9cf687965a7d8c89ef06bea9
luis <luis@gmail.com>
2021-10-18 22:52:35 +0200

    Getters and setters

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -29,13 +29,13 @@ class LinkedList:
     def size(self) -> int:
         return len([*self])
 
+    def get_head(self) -> Node:
+        return self.head
+
     def set_head(self, data: Any) -> None:
         node = self.Node(data)
         node.next = self.head
         self.head = node
-
-    def get_head(self) -> Node:
-        return self.head
 
     def pop_head(self) -> Node:
         if self.head:
@@ -46,18 +46,18 @@ class LinkedList:
         else:
             raise IndexError('pop from empty list')
 
+    def get_tail(self) -> Node:
+        if self.head:
+            *_, tail = self
+            return tail
+        return None
+
     def set_tail(self, data: Any) -> None:
         if self.head:
             *_, tail = self
             tail.next = self.Node(data)
         else:
             self.head = self.Node(data)
-
-    def get_tail(self) -> Node:
-        if self.head:
-            *_, tail = self
-            return tail
-        return None
 
     def pop_tail(self) -> Node:
         if self.head:
@@ -69,6 +69,14 @@ class LinkedList:
         else:
             raise IndexError('pop from empty list')
 
+    def get_node(self, pos: int) -> Any:
+        if (pos < 0) or (pos+1 > self.size()):
+            raise IndexError('index out of range')
+
+        for i, current in enumerate(self):
+            if i == pos:
+                return current
+                
     def set_node(self, pos: int, data: Any) -> None:
         node = self.Node(data)
         if not self.size():
@@ -88,14 +96,6 @@ class LinkedList:
         else:
             node.next = self.head
             self.head = node
-
-    def get_node(self, pos: int) -> Any:
-        if (pos < 0) or (pos+1 > self.size()):
-            raise IndexError('index out of range')
-
-        for i, current in enumerate(self):
-            if i == pos:
-                return current
 
     def pop_node(self, pos: int) -> None:
         if not self.size():
